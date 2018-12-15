e60d9561c5b5d5008c3f3c61a86992a4e7e54638
MikeWise2718 <mwise@microsoft.com>
2018-12-15 14:16:30 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TensorFlow;
+
+namespace tfsharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var session = new TFSession())
+			{
+				var graph = session.Graph;
+
+				var a = graph.Const(2);
+				var b = graph.Const(3);
+				Console.WriteLine("a=2 b=3");
+
+				// Add two constants
+				var addingResults = session.GetRunner().Run(graph.Add(a, b));
+				var addingResultValue = addingResults.GetValue();
+				Console.WriteLine("a+b={0}", addingResultValue);
+
+				// Multiply two constants
+				var multiplyResults = session.GetRunner().Run(graph.Mul(a, b));
+				var multiplyResultValue = multiplyResults.GetValue();
+				Console.WriteLine("a*b={0}", multiplyResultValue);
+			}
+        }
+    }
+}
